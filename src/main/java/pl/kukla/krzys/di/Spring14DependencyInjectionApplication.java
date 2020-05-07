5796fc44e34dcb1dbd8ef7662133716f85b3caf3
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-05-08 00:50:51 +0200

    Initial commit

---
+++ src/main/java/pl/kukla/krzys/di/Spring14DependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring14DependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring14DependencyInjectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kukla/krzys/di/Spring14DependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring14DependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
